c39082165819b4e2765c6785884390008034bb0d
junkieta <webmaster@junkieta.net>
2025-09-03 13:00:43 +0900

    "is" update

--- src/blooky-fc.ts
+++ src/blooky-fc.ts
@@ -1,9 +1,6 @@
 // blooky-fc.ts
 
-import { isChainedProp, isStream } from "./blooky-fp";
-
 // --- 型定義 ---
-
 type Blueprint<A extends Object> = { [key in keyof A]: PropertyDescriptor };
 
 const blueprint = <A extends Object>(o: A) : Blueprint<A> => {
@@ -14,21 +11,6 @@ const blueprint = <A extends Object>(o: A) : Blueprint<A> => {
         p = Object.getPrototypeOf(p);
     }
     return Object.assign({},...descs.reverse());
-}
-
-/**
- * 設計図の中から、指定されたキーを列挙不可に変更した、新しい設計図を返す。
- * @param keysToHide 隠蔽したいキーの配列
- * @param blueprint 元の設計図
- */
-const unenumerable = (keysToHide: string[]) => <A extends Object>(blueprint: Blueprint<A>) : Blueprint<A> => {
-  const newBlueprint = { ...blueprint };
-  for (const key of keysToHide) {
-    if (key in newBlueprint) {
-      newBlueprint[key] = { ...newBlueprint[key], enumerable: false };
-    }
-  }
-  return newBlueprint;
 }
 
 /**
@@ -53,33 +35,30 @@ const pick = <K extends string>(keys: K[]) => <A extends { [key in K]?: any }>(b
   return newBlueprint;
 }
 
+
 // 型検証器の名前空間
-const is = {
-    /**
-     * 値が nullableでないことを要求する
-     */
-    nonnull: (v: any) => v!=null,
-    /**
-     * 値が `string` 型であることを要求する Validator。
-     */
-    string: (v: any) => typeof v === "string",
-    /**
-     * 値が `number` 型であることを要求する Validator。
-     */
-    number: (v: any) => typeof v === "number",
-    /**
-     * 値が `Prop` であることを要求する Validator。
-     */
-    prop: isChainedProp,
-    /**
-     * 値が `Stream` であることを要求する Validator。
-     */
-    stream: isStream,
-    /**
-     * 値が `function` 型であることを要求する Validator。
-     */
-    function: (v: any) => typeof v === "function",
-    // ... boolean, object など、必要に応じて追加
+const is: { [key:string]: (v:any)=>boolean } = {
+  /**
+   * 値が nullableでないことを要求する
+   */
+  nonnull: (v: any): v is NonNullable<typeof v> => v!=null,
+  /**
+   * 値に関係なく、キーが存在していることを要求する
+   */
+  exists: (v: any) => true,
+  /**
+   * 値が `string` 型であることを要求する Validator。
+   */
+  string: (v: any): v is string => typeof v === "string",
+  /**
+   * 値が `number` 型であることを要求する Validator。
+   */
+  number: (v: any): v is number => typeof v === "number",
+  /**
+   * 値が `function` 型であることを要求する Validator。
+   */
+  function: (v: any): v is Function => typeof v === "function",
+  // ... boolean, object など、必要に応じて追加
 
 };
 
@@ -87,9 +66,8 @@ const is = {
 
 
 /**
- * 完成した設計図（ディスクリプタとオプションのProxy）から、
- * 最終的なコンテキストオブジェクトを生成（具現化）する。
- * ここで生成されるコンテキストは不変(frozen)となる。
+ * 完成した設計図（ディスクリプタとオプションのProxy）から、最終的なコンテキストオブジェクトを生成（具現化）する。
+ * 生成されるコンテキストは不変(frozen)となる。
  * @param spec プロパティディスクリプタで構成された設計図
  * @param options オプション（継承する親オブジェクト、契約を明示するバリデータオブジェクト）
  */
@@ -99,7 +77,7 @@ function build<T extends object>(
     parent?: object
     contract?: { [key: string]: (value: any) => boolean }
   } = {}
-): T {
+): Readonly<T> {
     // 仕様書（contract）が渡されていれば、検証を実行
     if (options.contract) {
         const contract = options.contract;
@@ -130,26 +108,31 @@ function build<T extends object>(
 
 /**
  * コンテキストを関数の第一引数に注入（prime）し、関数を実行する。
- * @param context 任意のコンテキストオブジェクト
+ * @param context 任意の凍結済みコンテキストオブジェクト
  * @param func コンテキストを第一引数として受け取る関数
  */
-const prime = <T extends object>(context: T) => <R>(func: (context: T) => R): R  => func(context);
+const prime = <T extends Readonly<object>>(context: T) => <R>(func: (context: T) => R): R  => func(context);
 
 /**
  * コンテキストを関数の`this`に宿らせ（embody）、新しい関数を返す。
- * @param context 任意のコンテキストオブジェクト
+ * @param context 任意の凍結済みコンテキストオブジェクト
  * @param func コンテキストを`this`として受け取る関数
  */
-const embody = <T extends object>(context: T) => <F extends (this: T, ...args: any[]) => any>(func: F): F => func.bind(context) as F;
+const embody = <T extends Readonly<object>>(context: T) => <F extends (this: T, ...args: any[]) => any>(func: F): F => func.bind(context) as F;
 
-// primeの引数順を逆にしたバージョン。テンプレートを宣言して、適したコンテキストを後から受け取って実行する。
-const template = <R,T extends object>(fn:(v:T)=>R) => (ctx:T) => fn(ctx);
+/**
+ * primeの引数順を逆にしたバージョン。テンプレートを宣言して、適したコンテキストを後から受け取って実行する。
+ * @param context 任意の凍結済みコンテキストオブジェクト
+ * @param func コンテキストを第一引数として受け取る関数
+ */
+const template = <R,T extends Readonly<object>>(fn:(v:T)=>R) => (ctx:T) => fn(ctx);
 
 
 export {
     Blueprint,
-    blueprint,build,prime,embody,template,
-    omit,pick,unenumerable,
+    blueprint,build,
+    omit,pick,
+    prime,embody,template,
     is
 }
 
